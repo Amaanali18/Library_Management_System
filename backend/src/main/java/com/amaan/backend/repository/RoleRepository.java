909f0158ce06a5c051aec01664b4ac8a116f4d2e
Amaanali Parchigar <amaanaliparchigar0918@gmail.com>
2026-08-06 21:31:49 +0530

    Started Building JWT Authentication Filter

--- backend/src/main/java/com/amaan/backend/repository/RoleRepository.java
+++ backend/src/main/java/com/amaan/backend/repository/RoleRepository.java
@@ -2,9 +2,12 @@ package com.amaan.backend.repository;
 
 import com.amaan.backend.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, UUID> {
     Optional<Role> findByName(String name);
 }
